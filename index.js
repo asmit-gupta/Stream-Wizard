b333c672d4ac42f8f6a91e8653dbfbb83e5f8f28
asmit-gupta <asmitwrites@gmail.com>
2024-05-05 03:35:55 +0530

    triggered stream on button press

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.use(express.static(path.resolve('./public')))
 
 io.on('connection', socket => {
     console.log(`Socket connected`, socket.id);
+    socket.on('binarystream', stream => {
+        console.log('Binary Stream Incomming...')
+    })
 })
 
-server.listen(3000, ()=> console.log(`HTTP SERVER RUNING ON PORT NO 3000 ✅`))
+server.listen(3000, ()=> console.log('HTTP SERVER RUNING ON PORT NO 3000 ✅'))
